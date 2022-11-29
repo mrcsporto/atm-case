4bf12700aab23de96bbe872dde0d11861197f136
mrcsporto <mrcsporto@gmail.com>
2022-11-29 16:40:13 -0300

    add validation to float balance

--- app/models/bank_account.rb
+++ app/models/bank_account.rb
@@ -1,20 +1,20 @@
 class BankAccount < ApplicationRecord
   belongs_to :client
-  
   has_many :account_transactions
 
   validates :client, presence: true
   validates :account_number, presence: true, uniqueness: true, length: { is: 6 }
-  validates :balance, presence: true,  numericality: {greater_than_or_equal_to: 0}
+  validates :balance, presence: true, numericality: { greater_than_or_equal_to: 0, less_than: BigDecimal(10**3) },
+                      format: { with: /\A\d{1,3}(\.\d{1,2})?\z/ }
 
   before_validation :new_account
 
   include Discard::Model
 
   def new_account
-    if self.new_record?
+    if new_record?
       self.balance = 0.00
-      self.account_number = ((SecureRandom.random_number(6e5) + 1e5).to_i).to_s
+      self.account_number = (SecureRandom.random_number(6e5) + 1e5).to_i.to_s
     end
   end
 end
