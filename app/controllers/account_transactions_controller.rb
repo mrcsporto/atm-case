8d4fbd991036a2e4c963aed084277594f7e2d335
mrcsporto <mrcsporto@gmail.com>
2022-11-29 16:37:26 -0300

    added features to accout transaction

--- app/controllers/account_transactions_controller.rb
+++ app/controllers/account_transactions_controller.rb
@@ -1,2 +1,52 @@
 class AccountTransactionsController < ApplicationController
+  before_action :set_account_transaction, only: %i[show edit update destroy]
+  before_action :set_bank_account, only: %i[show edit update destroy]
+
+  def index
+    @bank_accounts = BankAccount.all
+    @account_transactions = AccountTransaction.all
+  end
+
+  def new
+    @account_transaction = AccountTransaction.new
+  end
+
+  def show
+    @account_transaction = AccountTransaction.find(account_transaction_params[:id])
+    @bank_account = BankAccount.find(account_transaction_params[:bank_account_id])
+  end
+
+  def create
+    account = BankAccount.find(account_transaction_params[:bank_account_id])
+    @account_transaction = AccountTransaction.new(
+      amount: account_transaction_params[:amount],
+      transaction_type: account_transaction_params[:transaction_type],
+      bank_account: account
+    )
+    if @account_transaction.save
+      redirect_to transactions_path, notice: 'Bank account was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_bank_account
+    @bank_account = if params[:bank_account_id]
+                      BankAccount.find(params[:bank_account_id])
+                    else
+                      BankAccount.all
+                    end
+  end
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_account_transaction
+    @account_transaction = AccountTransaction.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def account_transaction_params
+    params.require(:account_transaction).permit(:transaction_type, :bank_account_id, :amount)
+  end
 end
--- app/models/account_transaction.rb
+++ app/models/account_transaction.rb
@@ -1,18 +1,27 @@
 class AccountTransaction < ApplicationRecord
   belongs_to :bank_account
 
-  TRANSACTION_TYPES = %w[transfer withdraw deposit]
+  TRANSACTION_TYPES = %w[Transfer Withdraw Deposit]
 
   validates :bank_account, presence: true
-  validates :amount, presence: true, numericality: {greater_than: 0}
-  validates :transaction_type, presence: true, inclusion: {in: TRANSACTION_TYPES}
+  validates :amount, presence: true, numericality: { greater_than: 0, less_than: BigDecimal(10**3) },
+                     format: { with: /\A\d{1,3}(\.\d{1,2})?\z/ }
+  validates :transaction_type, presence: true, inclusion: { in: TRANSACTION_TYPES }
   validates :transaction_number, presence: true, uniqueness: true
 
   before_validation :transaction_uniq_id
+  before_save :upcase_transaction_type
+  before_save :set_amount
+
+  def upcase_transaction_type
+    self.transaction_type = transaction_type.upcase
+  end
+
+  def set_amount
+    amount.to_f
+  end
 
   def transaction_uniq_id
-    if self.new_record?
-      self.transaction_number = SecureRandom.uuid
-    end
+    self.transaction_number = SecureRandom.uuid if new_record?
   end
 end
