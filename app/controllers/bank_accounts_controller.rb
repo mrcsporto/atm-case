371669103b3690ff3b0d4423c7b20115589bafe1
mrcsporto <mrcsporto@gmail.com>
2022-11-29 16:39:56 -0300

    fix format

--- app/controllers/bank_accounts_controller.rb
+++ app/controllers/bank_accounts_controller.rb
@@ -6,7 +6,7 @@ class BankAccountsController < ApplicationController
   # GET /bank_accounts or /bank_accounts.json
   def index
     client = Client.find(session[:client_id]) if session[:client_id]
-      if client.role === "admin"
+      if client.role === 'admin'
         @bank_accounts = BankAccount.all
       else
         @bank_accounts = BankAccount.kept
@@ -30,63 +30,54 @@ class BankAccountsController < ApplicationController
   # POST /bank_accounts or /bank_accounts.json
   def create
     client = Client.find(session[:client_id]) if session[:client_id]
-    if client.role === "admin"
+    if client.role === 'admin'
       @bank_account = BankAccount.new(bank_account_params)
     else
       @bank_account = BankAccount.new(client_id: client.id)
     end
-    respond_to do |format|
       if @bank_account.save
-        format.html { redirect_to bank_account_url(@bank_account), notice: "Bank account was successfully created." }
-        format.json { render :show, status: :created, location: @bank_account }
+        redirect_to bank_account_url(@bank_account), notice: 'Bank account was successfully created.'
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @bank_account.errors, status: :unprocessable_entity }
+        render :new, status: :unprocessable_entity
       end
-    end
   end
 
   # PATCH/PUT /bank_accounts/1 or /bank_accounts/1.json
   def update
-    respond_to do |format|
       if @bank_account.update(bank_account_edit_params)
-        format.html { redirect_to bank_account_url(@bank_account), notice: "Bank account was successfully updated." }
-        format.json { render :show, status: :ok, location: @bank_account }
+       redirect_to bank_account_url(@bank_account), notice: 'Bank account was successfully updated.'
       else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @bank_account.errors, status: :unprocessable_entity }
+        render :edit, status: :unprocessable_entity
       end
-    end
   end
 
   # DELETE /bank_accounts/1 or /bank_accounts/1.json
   def destroy
     @bank_account.discard
-
-    respond_to do |format|
-      format.html { redirect_to bank_accounts_url, notice: "Bank account was successfully archived." }
-      format.json { head :no_content }
-    end
+      redirect_to bank_accounts_url, notice: 'Bank account was successfully archived.'
   end
 
   private
-    def set_client
-      if params[:client_id]
-        @client = Client.find(params[:client_id])
-      else
-        @client = Client.all
-      end
-    end
-    # Use callbacks to share common setup or constraints between actions.
-    def set_bank_account
-      @bank_account = BankAccount.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def bank_account_params
-      params.require(:bank_account).permit(:client_id, :balance, :account_number)
+  def set_client
+    if params[:client_id]
+      @client = Client.find(params[:client_id])
+    else
+      @client = Client.all
     end
-    def bank_account_edit_params
-      params.permit(:client_id, :balance, :account_number)
-    end
+  end
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_bank_account
+    @bank_account = BankAccount.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def bank_account_params
+    params.require(:bank_account).permit(:client_id, :balance, :account_number)
+  end
+
+  def bank_account_edit_params
+    params.permit(:client_id, :full_name, :balance, :account_number)
+  end
 end
