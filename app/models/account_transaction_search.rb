e86a324b34ee8be01fe938bf4aebabf69977efd3
mrcsporto <mrcsporto@gmail.com>
2022-11-30 17:41:19 -0300

    adjust date format

--- app/models/account_transaction_search.rb
+++ app/models/account_transaction_search.rb
@@ -3,8 +3,8 @@ class AccountTransactionSearch
 
   def initialize(params)
     params ||= {  }
-    @date_from = parsed_date(params[:date_from], 7.days.ago.to_date.to_s)
-    @date_to = parsed_date(params[:date_to], Date.today.to_s)
+    @date_from = parsed_date(params[:date_from], 7.days.ago.to_s(:db).split(' ')[0]  )
+    @date_to = parsed_date(params[:date_to], Date.today.to_s(:db).split(' ')[0]  )
   end
 
   def scope
