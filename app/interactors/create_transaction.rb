1ef7718f1eda4d6bca55d007ed51f6890437302e
mrcsporto <mrcsporto@gmail.com>
2022-12-07 19:23:31 -0300

    add transfer transation

--- app/interactors/create_transaction.rb
+++ app/interactors/create_transaction.rb
@@ -6,23 +6,21 @@ class CreateTransaction
     bank_account = BankAccount.find(context.params[:bank_account_id])
     amount = context.params[:amount].to_f
     balance = BankAccount.find(context.params[:bank_account_id]).balance.to_f
-
+    receiver_account = BankAccount.find(context.params[:receiver_id]) if receiver_account.nil?
+    
     context.account_transaction = AccountTransaction.new(context.params)
-
-    # if transaction_type == 'Transfer' && bank_account.balance - amount < 0.00
-    #   receiver_account = BankAccount.find(context.params[:receiver_id])
-    #   BankAccount.update(bank_account.id, balance: balance - amount)
-    #   BankAccount.update(receiver_account.id, balance: receiver_account.balance.to_f + amount)
-    # else
-    #   context.fail! unless context.account_transaction.valid?
-    # end
-
+    
+ 
     if transaction_type == 'Withdraw' && bank_account.balance >= amount
       context.account_transaction.save
       BankAccount.update(bank_account.id, balance: balance - amount)
     elsif transaction_type == 'Deposit' && amount.positive?
       context.account_transaction.save
       BankAccount.update(bank_account.id, balance: balance + amount)
+    elsif transaction_type == 'Transfer' && bank_account.balance - amount >= 0.00
+      context.account_transaction.save
+      BankAccount.update(bank_account.id, balance: balance - amount)
+      BankAccount.update(receiver_account.id, balance: receiver_account.balance.to_f + amount)
     else
       context.fail!
     end
