3a49b3c3ce1d4cd8e388b9409e734953fb5b18cf
mrcsporto <mrcsporto@gmail.com>
2022-12-07 20:04:11 -0300

    fix transfer operation

--- app/interactors/create_transaction.rb
+++ app/interactors/create_transaction.rb
@@ -6,19 +6,17 @@ class CreateTransaction
     bank_account = BankAccount.find(context.params[:bank_account_id])
     amount = context.params[:amount].to_f
     balance = BankAccount.find(context.params[:bank_account_id]).balance.to_f
-    receiver_account = BankAccount.find(context.params[:receiver_id]) if receiver_account.nil?
-    
     context.account_transaction = AccountTransaction.new(context.params)
-    
- 
+    context.account_transaction.save
+
     if transaction_type == 'Withdraw' && bank_account.balance >= amount
-      context.account_transaction.save
       BankAccount.update(bank_account.id, balance: balance - amount)
+
     elsif transaction_type == 'Deposit' && amount.positive?
-      context.account_transaction.save
       BankAccount.update(bank_account.id, balance: balance + amount)
+
     elsif transaction_type == 'Transfer' && bank_account.balance - amount >= 0.00
-      context.account_transaction.save
+      receiver_account = BankAccount.find(context.params[:receiver_id]) unless receiver_account.present?
       BankAccount.update(bank_account.id, balance: balance - amount)
       BankAccount.update(receiver_account.id, balance: receiver_account.balance.to_f + amount)
     else
